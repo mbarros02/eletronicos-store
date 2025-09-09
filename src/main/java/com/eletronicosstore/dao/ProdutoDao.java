f33aaac90d87339c3226aef678f4921c5599e0f5
NickMLN <moreiralima.nicolas@gmail.com>
2025-09-09 19:06:46 -0300

    Adicionado classe "ProdutoDao"

---
+++ src/main/java/com/eletronicosstore/dao/ProdutoDao.java
@@ -0,0 +1,59 @@
+package com.eletronicosstore.dao;
+
+public class ProdutoDao {
+    private String nome;
+    private double avaliacao;
+    private String descricao;
+    private double preco;
+    private int qtdEstoque;
+    private String imagem;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
+
