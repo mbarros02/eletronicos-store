f703ff427e0dc5350b579220e917f9ff5377508a
NickMLN <moreiralima.nicolas@gmail.com>
2025-10-09 07:30:01 -0300

    Refatoração scricpt-database

--- src/main/java/com/eletronicosstore/database/script-database.sql
+++ src/main/java/com/eletronicosstore/database/script-database.sql
@@ -3,13 +3,13 @@ CREATE DATABASE IF NOT EXISTS eletronicos_store;
 USE eletronicos_store;
 
 -- Criação da tabela de grupos de usuários
-CREATE TABLE IF NOT EXISTS grupo_usuario (
+CREATE TABLE grupo_usuario (
     IDGRUPO INT PRIMARY KEY AUTO_INCREMENT,
     NOME ENUM('ADMINISTRADOR', 'ESTOQUISTA') NOT NULL
 );
 
 -- Criação da tabela de usuários
-CREATE TABLE IF NOT EXISTS usuarios (
+CREATE TABLE usuarios (
     IDUSUARIO INT PRIMARY KEY AUTO_INCREMENT,
     NOME VARCHAR(255) NOT NULL,
     CPF CHAR(14) NOT NULL,
@@ -21,29 +21,21 @@ CREATE TABLE IF NOT EXISTS usuarios (
 );
 
 -- Criação da tabela de produtos
-CREATE TABLE IF NOT EXISTS produtos (
+CREATE TABLE produtos (
     IDPRODUTO INT PRIMARY KEY AUTO_INCREMENT,
     NOME VARCHAR(200) NOT NULL,
     AVALIACAO DECIMAL(2,1) CHECK (AVALIACAO BETWEEN 1 AND 5),
     DESCRICAO TEXT,
     PRECO DECIMAL(10,2),
-    QTD_ESTOQUE INT
+    QTD_ESTOQUE INT,
+    STATUS TINYINT(1) DEFAULT 1
 );
 
 -- Criação da tabela de imagens dos produtos
-CREATE TABLE IF NOT EXISTS imagem_produto (
+CREATE TABLE imagem_produto (
     IDIMAGEM INT PRIMARY KEY AUTO_INCREMENT,
     CAMINHO VARCHAR(255) NOT NULL,
     PRINCIPAL BOOLEAN DEFAULT FALSE,
     ID_PRODUTO INT,
     FOREIGN KEY (ID_PRODUTO) REFERENCES produtos(IDPRODUTO)
 );
-
--- Adiciona a coluna status se ainda não existir
-ALTER TABLE produtos ADD COLUMN status TINYINT(1) DEFAULT 1;
-
--- Ativa todos os produtos
-UPDATE produtos SET status = 1;
-UPDATE produtos SET status=0 WHERE idproduto=1;
-UPDATE produtos SET status=1 WHERE idproduto=1;
-
